8df127f6568c0e7418e53ec2eae41dac1b9f1903
Nuno Maduro <enunomaduro@gmail.com>
2023-04-18 00:27:44 +0100

    tests: removes removed `--exclude` option

--- tests/Finder/FinderTest.php
+++ tests/Finder/FinderTest.php
@@ -17,33 +17,6 @@ it('find test file in multiple directories', function () {
     expect($finder->count())->toEqual(4);
 });
 
-it('does not return excluded file', function () {
-    $finder = (new Finder(tmpDir('sources/Alpha'), 'WorldTest.php'));
-
-    expect($finder->count())->toEqual(1);
-});
-
-it('does not return files in excluded directories', function () {
-    $finder = (new Finder(tmpDir('sources/Beta'), 'Charlie'));
-
-    expect($finder->count())->toEqual(1);
-});
-
-it('does not return multiple excluded path', function () {
-    $finder = (new Finder(
-        [
-            tmpDir('sources/Alpha'),
-            tmpDir('sources/Beta'),
-        ],
-        [
-            'HelloTest.php',
-            'Charlie',
-        ]
-    ));
-
-    expect($finder->count())->toEqual(2);
-});
-
 it('does not return non test files', function () {
     $files = (new Finder(tmpDir('sources')))->get();
 
